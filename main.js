8204fee9f4a161c282c212a76f30095fb84a80f2
risonakamo <hmykanae@gmail.com>
2017-08-26 00:44:05 -0400

    selected state style

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 window.onload=main;
 
+var _soundboxes;
+
 function main()
 {
+    _soundboxes=document.querySelectorAll("sound-box");
 
+    var stopAll=document.querySelector(".stop-button");
+
+    stopAll.addEventListener("click",(e)=>{
+        for (var x=0;x<_soundboxes.length;x++)
+        {
+            _soundboxes[x].vstop();
+        }
+    });
 }
